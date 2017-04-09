acb4bc2c50c34aa70f1094f64d5d61062fdc2e88
baiyangcao <baiyangcao@msn.cn>
2017-04-09 22:20:47 +0800

    modify notesbuilder.py add index.html generate

--- notesbuilder.py
+++ notesbuilder.py
@@ -1,8 +1,10 @@
 # -*- coding: UTF-8 -*-
 import scrapy
-import urllib.parse
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from os import path
+from urllib.parse import urljoin
+
 
 class NotesSpider(scrapy.Spider):
     name = 'Notes'
@@ -21,7 +23,7 @@ class NotesSpider(scrapy.Spider):
         for post in response.css('#main-content h1'):
             json = {
                 'text': post.css('a::text').extract_first(),
-                'url': urllib.parse.urljoin(self.start_urls[0], post.css('a::attr("href")').extract_first())
+                'url': urljoin(self.start_urls[0], post.css('a::attr("href")').extract_first())
             }
             # yield json
             self.data.append(json)
@@ -34,6 +36,13 @@ class NotesSpider(scrapy.Spider):
     def close(self, reason):
         readme_tempalte = self.env.get_template('README.md')
         readme = readme_tempalte.render(notes=self.data)
-        filename = getattr(self, 'filename', 'READEME.md')
-        with open(filename, 'w') as file:
+        readme_name = getattr(self, 'readme', 'READEME.md')
+        with open(readme_name, 'w') as file:
             file.write(readme)
+
+        index_template = self.env.get_template('index.html')
+        index = index_template.render(notes=self.data)
+        index_name = getattr(self, 'index', 'index.html')
+        index_path = path.join('docs', index_name)
+        with open(index_path, 'w') as file:
+            file.write(index)
