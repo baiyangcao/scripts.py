efccd4c0a42044c371f3e351906914f46e536ce9
baiyangcao <baiyangcao@msn.cn>
2017-05-08 14:42:13 +0800

    modify blogformatter.py add type argument to get the target format type

--- blogformatter.py
+++ blogformatter.py
@@ -5,16 +5,29 @@ import os
 from optparse import OptionParser
 
 
-def formatline(line):
+def formatline(line, type):
     """
-    format line, change ```javascript to {% highlight javascript %}
+    format line according to type, change code style to markdown or rouge syntax
     :param line: raw line string 
+    :param type: target format type, available value: markdown, rouge 
     :return: formatted line string
     """
-    start_regexp = r"^\s*```(.+)\s*$"
-    start_replace = r"{% highlight \1 %}\n"
-    end_regexp = r"^\s*```\s*$"
-    end_replace = r"{% endhighlight %}\n"
+    md_start_regexp = r"^\s*```(.+)\s*$"
+    rouge_start_regexp = r"{% highlight \1 %}\n"
+    md_end_regexp = r"^\s*```\s*$"
+    rouge_end_regexp = r"{% endhighlight %}\n"
+
+    if type.lower() == "markdown":
+        start_regexp = md_start_regexp
+        start_replace = rouge_start_regexp
+        end_regexp = md_end_regexp
+        end_replace = rouge_start_regexp
+    elif type.lower() == "rouge":
+        start_regexp = rouge_start_regexp
+        start_replace = md_start_regexp
+        end_regexp = rouge_end_regexp
+        end_replace = md_end_regexp
+
     if re.fullmatch(start_regexp, line) is not None:
         return re.sub(start_regexp, start_replace, line)
     elif re.fullmatch(end_regexp, line) is not None:
@@ -27,6 +40,10 @@ def main():
     parser = OptionParser("usage: %prog [options] arg")
     parser.add_option("-f", "--file", dest="filename",
                       help="the file want to be formatted")
+    parser.add_option("-t", "--type", dest="type",
+                      type="choice", default="rouge",
+                      choices=("markdown", "rouge"),
+                      help="target format type, available values: markdown, rouge")
     (options, args) = parser.parse_args()
     if len(args) < 0 or not options.filename:
         print("Please input the file want to be formatted")
@@ -35,7 +52,7 @@ def main():
         lines = []
         with open(options.filename, 'r', encoding='utf-8') as f:
             for line in iter(f.readline, ''):
-                lines.append(formatline(line))
+                lines.append(formatline(line, options.type))
 
         with open(options.filename, 'w', encoding='utf-8') as f:
             f.writelines(lines)
