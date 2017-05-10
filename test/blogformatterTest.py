e193582dc6fba5062b82c10a4dc9ad62ea57e3b7
baiyangcao <baiyangcao@msn.cn>
2017-05-10 22:14:16 +0800

    add formateline rouge test

--- test/blogformatterTest.py
+++ test/blogformatterTest.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from unittest import TestCase
+import sys
+
+sys.path.append('..')
+
+from unittest import TestCase, main
+from blogformatter import formatline
 
 
 class BlogFormatterTest(TestCase):
-    def test_formateline(self):
-        pass
+    def test_formateline_start_rouge(self):
+        '''
+        formateline function test
+        target markdown syntax
+        :return: 
+        '''
+        source = '```javascript'
+        result = '{% highlight javascript %}'
+        self.assertEqual(result, formatline(source, 'rouge').strip('\n'))
+
+if __name__ == '__main__':
+    main()
