3bd5135edb4a292d6212732fc34fbf40b6624902
baiyangcao <baiyangcao@msn.cn>
2017-05-11 22:31:36 +0800

    add formateline rouge syntax code-range-end unit test

--- test/blogformatterTest.py
+++ test/blogformatterTest.py
@@ -12,11 +12,23 @@ class BlogFormatterTest(TestCase):
     def test_formateline_start_rouge(self):
         '''
         formateline function test
-        target markdown syntax
+        target rouge syntax
+        code range start
         :return: 
         '''
         source = '```javascript'
         result = '{% highlight javascript %}'
+        self.assertEqual(result, formatline(source, 'rouge').strip('\n'))
+
+    def test_formateline_end_rouge(self):
+        '''
+        formateline function test
+        target rouge syntax
+        code range end
+        :return: 
+        '''
+        source = '```'
+        result = '{% endhighlight %}'
         self.assertEqual(result, formatline(source, 'rouge').strip('\n'))
 
 if __name__ == '__main__':
