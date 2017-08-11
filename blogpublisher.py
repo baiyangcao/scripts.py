ff1382cfd10497f9b2349ba176cd285a50d3e8ee
baiyangcao <baiyangcao@msn.cn>
2017-08-11 21:45:11 +0800

    添加 cnblog 博客园博文发布类实现

--- blogpublisher.py
+++ blogpublisher.py
@@ -4,20 +4,25 @@
 publish blog to blog site, cnblog or oschina
 """
 
+import xmlrpc.client
 
 class BlogPublisher:
     """
     base class for blog publish
     """
 
-    def __init__(self):
-        self.username = ""
-        self.password = ""
-        self.baseurl = ""
-        self.loginurl = ""
-        self.publishurl = ""
+    def __init__(self, username, password, url):
+        """
+        initialize the username, password and mateWeblog url
+        :param username:
+        :param password:
+        :param url: mateWeblog url
+        """
+        self.username = username
+        self.password = password
+        self.url = url
 
-    def publish(self, content):
+    def publish(self, title, content):
         """
         publish blog to 
         :param content: blog content to publish (markdown syntax)
@@ -32,15 +37,24 @@ class CnBlogPublisher(BlogPublisher):
     """
 
     def __init__(self):
-        super.__init__()
+        super("baiyangcao", "y2211612", "http://rpc.cnblogs.com/metaweblog/baiyangcao")
+        self.appKey = "baiyangcao"
+        self.server = xmlrpc.client.ServerProxy(self.url)
+        info = self.server.blogger.getUsersBlogs(self.appKey, self.username, self.password)
+        self.blogid = info[0]["blogid"]
 
-    def publish(self, content):
+    def publish(self, title, content):
         """
         publish blog to 
         :param content: blog content to publish (markdown syntax)
         :return: 
         """
-        pass
+        post = {
+            "title": title,
+            "description": content
+        }
+        self.server.metaWeblog.newPost(self.blogid, self.username, self.password, post, False)
+
 
 
 class OsChinaPublisher(BlogPublisher):
@@ -51,7 +65,7 @@ class OsChinaPublisher(BlogPublisher):
     def __init__(self):
         super.__init__()
 
-    def publish(self, content):
+    def publish(self, title, content):
         """
         publish blog to 
         :param content: blog content to publish (markdown syntax)
