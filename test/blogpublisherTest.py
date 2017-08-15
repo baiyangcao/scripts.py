dd89384776c484df6fc3df19d3e170c5b6585fdf
baiyangcao <baiyangcao@msn.cn>
2017-08-15 21:15:37 +0800

    添加 oschina 调用 getUsersBlogs 测试方法

--- test/blogpublisherTest.py
+++ test/blogpublisherTest.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import xmlrpc
 from unittest import TestCase
 from blogpublisher import CnBlogPublisher;
 
@@ -15,3 +16,13 @@ class BlogPublisherTest(TestCase):
         """
         cnblog = CnBlogPublisher()
         cnblog.publish("test", "this is a test")
+
+    def test_oschina_getUsersBlogs(self):
+        url = "https://my.oschina.net/action/xmlrpc"
+        appKey = "baiyangcao"
+        username = "497462386@qq.com"
+        password = "y2211612"
+        transport = xmlrpc.client.SafeTransport()
+        transport.user_agent = "Fiddler"
+        server = xmlrpc.client.ServerProxy(url, transport=transport)
+        print(server.blogger.getUsersBlogs(appKey, username, password))
