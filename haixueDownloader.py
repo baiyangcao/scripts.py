8c6f771cdb6c0e8c7f111046968fc197fbac6a84
baiyangcao <baiyangcao@msn.cn>
2017-08-31 18:33:10 +0800

    HaiXue 类添加多线程下载方法，提高下载速度

--- haixueDownloader.py
+++ haixueDownloader.py
@@ -1,5 +1,8 @@
 # -*- encoding: utf-8 -*-
 import os
+import threading
+import traceback
+
 import requests
 
 
@@ -52,7 +55,77 @@ class HaiXue():
         self.__check_path(path)
         filepath = os.path.join(path, name + format)
         print('开始下载：', filepath)
-        # download
+        self.__multithreading_download(resp.headers['Location'], filepath)
+
+    def __multithreading_download(self, url, filepath, num=8):
+        """
+        download file by multithreading
+        :param url:
+        :param filepath: local file path including file name
+        :param num: threads count
+        :return:
+        """
+        try:
+            # 获取文件总大小
+            r = requests.head(url, cookies=self.cookies)
+            total = int(r.headers['Content-Length'])
+
+            # 检查是否需要下载文件
+            if os.path.exists(filepath):
+                # 检查本地文件大小是否符合，若符合则跳过，否则删除重新下载
+                if os.path.getsize(filepath) == total:
+                    print('跳过文件：', filepath)
+                    return
+                else:
+                    os.remove(filepath)
+
+            # 获取每一块的下载范围
+            ranges = []
+            offset = int(total / num)
+            for i in range(num):
+                if i == num - 1:
+                    ranges.append((i * offset, ''))
+                else:
+                    ranges.append((i * offset, (i + 1) * offset))
+
+            # 开始下载
+            with open(filepath, 'wb') as fd:
+                thread_list = []
+                n = 0
+                for ran in ranges:
+                    start, end = ran
+                    n += 1
+                    thread = threading.Thread(target=self.__download, args=(url, fd, start, end))
+                    thread.start()
+                    thread_list.append(thread)
+                for i in thread_list:
+                    i.join()
+
+            print('成功下载：', filepath)
+        except:
+            print('下载失败：', filepath)
+            traceback.print_exc()
+
+    def __download(self, url, fd, start, end):
+        """
+        download method for each range
+        :param start:
+        :param end:
+        :return:
+        """
+        headers = {'Range': 'Bytes=%s-%s' % (start, end), 'Accept-Encoding': '*'}
+        res = requests.get(url, headers=headers, cookies=self.cookies)
+        fd.seek(start)
+        fd.write(res.content)
+
+    def __check_path(self, path):
+        """
+        check whether the `path` exists, if not create it
+        :param path:
+        :return:
+        """
+        if not os.path.exists(path):
+            os.makedirs(path)
 
 
 if __name__ == '__main__':
